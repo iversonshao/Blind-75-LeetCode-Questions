3f23a231710489acbcb078ffd7de44e9fcbe23f3
LAPTOP-891TQN02\iversonshao <whistle327@gmail.com>
2023-03-17 17:27:41 +0800

    version1.1

--- Array/best_time_to_buy_and_sell_stock.py
+++ Array/best_time_to_buy_and_sell_stock.py
@@ -9,24 +9,24 @@ class Solution:
         for i in range(1, len(prices)):
             # checking for lower buy value
             # way1
-            # if (buy > prices[i]):
-            #     buy = prices[i]
+            if (buy > prices[i]):
+                buy = prices[i]
 
             # checking for higher profit
-            # elif (prices[i] - buy > max_profit):
-            #     max_profit = prices[i] - buy
-            # way2
-            buy = min(buy, prices[i])
-            max_profit = max(max_profit, prices[i] - buy)
+            elif (prices[i] - buy > max_profit):
+                max_profit = prices[i] - buy
+            # # way2
+            # buy = min(buy, prices[i])
+            # max_profit = max(max_profit, prices[i] - buy)
         return max_profit
 
 
 # case1
-# sol = Solution()
-# res = sol.maxProfit([7, 1, 5, 3, 6, 4])
-# print(res)
+sol = Solution()
+res = sol.maxProfit([7, 1, 5, 3, 6, 4])
+print(res)
 
 # case2
-sol = Solution()
-res = sol.maxProfit([7, 6, 4, 3, 1])
-print(res)
+# sol = Solution()
+# res = sol.maxProfit([7, 6, 4, 3, 1])
+# print(res)
