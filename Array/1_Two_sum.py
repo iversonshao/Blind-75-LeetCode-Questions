8886062a4c28342e4f534dfd4946d45cf420ee96
LAPTOP-891TQN02\iversonshao <whistle327@gmail.com>
2023-03-29 17:35:11 +0800

    version3.0

--- Array/1_Two_sum.py
+++ Array/1_Two_sum.py
@@ -6,23 +6,33 @@
 
 class Solution(object):
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        # way1
+        # for i in range(len(nums)):
+        #     for j in range(i+1, len(nums)):
+        #         if nums[i] + nums[j] == target:
+        #             return [i, j]
+        # way2 hashtable
+        d = {}
         for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+            num1 = nums[i]
+            num2 = target - num1
+            if num2 in d:
+                return [d[num2], i]
+            d[num1] = i
 
 
 # case1
-# sol = Solution()
-# res = sol.twoSum([2, 7, 11, 15], 9)
-# print(res)
+sol = Solution()
+res = sol.twoSum([2, 7, 11, 15], 26)
+print(res)
 
 # case2
 # sol = Solution()
 # res = sol.twoSum([3, 2, 4], 6)
 # print(res)
 
+
 # case3
-sol = Solution()
-res = sol.twoSum([3, 3], 6)
-print(res)
+# sol = Solution()
+# res = sol.twoSum([3, 3], 6)
+# print(res)
